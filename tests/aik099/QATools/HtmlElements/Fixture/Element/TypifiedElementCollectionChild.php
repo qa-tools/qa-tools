e32c5a4090189a6429320b434074a538b14e571f
Alexander Obuhovich <aik.bold@gmail.com>
2014-05-16 23:31:02 +0300

    Introducing AbstractElementCollection class
    Introducing AbstractElementCollection class to ease element array manipulations

---
+++ tests/aik099/QATools/HtmlElements/Fixture/Element/TypifiedElementCollectionChild.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the qa-tools library.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @copyright Alexander Obuhovich <aik.bold@gmail.com>
+ * @link      https://github.com/aik099/qa-tools
+ */
+
+namespace tests\aik099\QATools\HtmlElements\Fixture\Element;
+
+
+use aik099\QATools\HtmlElements\Element\AbstractTypifiedElementCollection;
+
+class TypifiedElementCollectionChild extends AbstractTypifiedElementCollection
+{
+
+	/**
+	 * Initializes collection with a list of elements.
+	 *
+	 * @param array $elements Elements.
+	 */
+	public function __construct(array $elements = array())
+	{
+		$this->elementClass = '\\aik099\\QATools\\HtmlElements\\Element\\TextInput';
+
+		parent::__construct($elements);
+	}
+
+}
