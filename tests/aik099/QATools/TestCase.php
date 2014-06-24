04241ea151e183c95e3f330442cf8d83dfb65008
Alexander Obuhovich <aik.bold@gmail.com>
2014-06-24 15:54:39 +0300

    Updating url to the repo
    Closes #71

--- tests/aik099/QATools/TestCase.php
+++ tests/aik099/QATools/TestCase.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * This file is part of the qa-tools library.
+ * This file is part of the QA-Tools library.
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  *
  * @copyright Alexander Obuhovich <aik.bold@gmail.com>
- * @link      https://github.com/aik099/qa-tools
+ * @link      https://github.com/qa-tools/qa-tools
  */
 
 namespace tests\aik099\QATools;
