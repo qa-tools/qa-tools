773a5e4ffafce7d3bac7e5604dc8d4fd4956e7ec
Alexander Obuhovich <aik.bold@gmail.com>
2014-07-04 20:02:57 +0300

    Live tests were accessing RadioGroup not as element collection

--- tests/QATools/QATools/HtmlElementsLive/Element/RadioGroupTest.php
+++ tests/QATools/QATools/HtmlElementsLive/Element/RadioGroupTest.php
@@ -28,33 +28,45 @@ class RadioGroupTest extends TypifiedElementTestCase
 	public function testGetButtonsWithName()
 	{
 		/* @var $radio_group RadioGroup */
-		$radio_group = $this->createElement(array('xpath' => "(//html/descendant-or-self::*[@id = 'r1_v3'])[1]"));
-		$buttons = $radio_group->getButtons();
+		$radio_group = $this->createElement(array('xpath' => "/descendant-or-self::*[@name = 'radio-group1']"));
 
-		$this->assertCount(4, $buttons);
-		$this->assertInstanceOf(self::RADIO_CLASS, $buttons[0]);
+		$this->assertCount(4, $radio_group);
+		$this->assertInstanceOf(self::RADIO_CLASS, $radio_group[0]);
 	}
 
 	public function testGetButtonsWithoutName()
 	{
 		/* @var $radio_group RadioGroup */
 		$radio_group = $this->createElement(array(
-			'xpath' => "(//html/descendant-or-self::*[@id = 'radio-without-group'])[1]",
+			'xpath' => "/descendant-or-self::*[@type = 'radio']",
 		));
-		$buttons = $radio_group->getButtons();
 
-		$this->assertCount(5, $buttons);
-		$this->assertInstanceOf(self::RADIO_CLASS, $buttons[0]);
+		$this->assertCount(5, $radio_group);
+		$this->assertInstanceOf(self::RADIO_CLASS, $radio_group[0]);
 	}
 
 	public function testSelection()
 	{
 		/* @var $radio_group RadioGroup */
-		$radio_group = $this->createElement(array('xpath' => "(//html/descendant-or-self::*[@id = 'r1_v3'])[1]"));
+		$radio_group = $this->createElement(array('xpath' => "/descendant-or-self::*[@name = 'radio-group1']"));
 
 		$this->assertFalse($radio_group->hasSelectedButton(), 'No radio button is selected initially');
 		$radio_group->selectButtonByValue(4);
 		$this->assertEquals(4, $radio_group->getSelectedButton()->getValue());
+	}
+
+	/**
+	 * Creates element.
+	 *
+	 * @param array $selector Selector.
+	 *
+	 * @return RadioGroup
+	 */
+	protected function createElement(array $selector)
+	{
+		$node_elements = $this->session->getPage()->findAll('se', $selector);
+
+		return call_user_func($this->elementClass . '::fromNodeElements', $node_elements, null, $this->pageFactory);
 	}
 
 }
