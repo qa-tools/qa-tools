eed794e26dc080fb4e7ebd6e5fd38f14cc6b1139
Alexander Obuhovich <aik.bold@gmail.com>
2014-07-05 13:05:14 +0300

    Adding AbstractFormElement marker class

--- library/QATools/QATools/HtmlElements/Element/SelectOption.php
+++ library/QATools/QATools/HtmlElements/Element/SelectOption.php
@@ -16,7 +16,7 @@ use QATools\QATools\HtmlElements\Exception\SelectException;
 /**
  * Represents one option in a web page select control. Please use "Select" typified element for manipulations.
  */
-class SelectOption extends AbstractTypifiedElement
+class SelectOption extends AbstractFormElement
 {
 
 	/**
