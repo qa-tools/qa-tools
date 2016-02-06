0246753de0b91fce58cccca32103c29e132bd02d
Alexander Obuhovich <aik.bold@gmail.com>
2016-02-06 20:33:39 +0200

    Use latest Selenium Server version

--- tests/run_selenium.sh
+++ tests/run_selenium.sh
@@ -7,7 +7,7 @@ sh -e /etc/init.d/xvfb start
 sleep 4
 
 echo "Downloading Selenium"
-curl -L http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar > selenium.jar
+curl -L http://selenium-release.storage.googleapis.com/2.51/selenium-server-standalone-2.51.0.jar > selenium.jar
 
 echo "Running Selenium"
 java -jar selenium.jar > /dev/null 2> /tmp/webdriver_output.txt &
