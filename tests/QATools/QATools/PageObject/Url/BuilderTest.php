f68617780d8dac729f6f9f79399c21f356716923
Khair-ed-Din Husseini <khairi.husseini@sensiolabs.de>
2016-06-07 14:35:11 +0200

    Implemented @page-url unmasking

--- tests/QATools/QATools/PageObject/Url/BuilderTest.php
+++ tests/QATools/QATools/PageObject/Url/BuilderTest.php
@@ -172,6 +172,117 @@ class BuilderTest extends TestCase
 				array(),
 				'https://domain.tld/path',
 			),
+			'one parameter unmasked' => array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{replace_me}',
+					'port' => 80,
+				),
+				array('replace_me' => 'i-was-replaced'),
+				'http://domain.tld/path/i-was-replaced',
+			),
+			'unmasked parameter and query string' => array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{replace_me}',
+					'port' => 80,
+				),
+				array(
+					'replace_me' => 'i-was-replaced',
+					'more_params' => 'some-query-string-value',
+				),
+				'http://domain.tld/path/i-was-replaced?more_params=some-query-string-value',
+			),
+			'two parameters unmasked' => array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{replace_me}/{2nd_replace}',
+					'port' => 80,
+				),
+				array(
+					'replace_me' => 'i-was-replaced',
+					'2nd_replace' => 'it-works',
+				),
+				'http://domain.tld/path/i-was-replaced/it-works',
+			),
+			'broken mask not unmasked' => array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{i_am_broken/{replace_me}',
+					'port' => 80,
+				),
+				array('replace_me' => 'i-was-replaced'),
+				'http://domain.tld/path/{i_am_broken/i-was-replaced',
+			),
+			'duplicate mask unmasked' => array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{replace_me}/{replace_me}',
+					'port' => 80,
+				),
+				array('replace_me' => 'i-was-replaced'),
+				'http://domain.tld/path/i-was-replaced/i-was-replaced',
+			),
+			'optional parameter unmasking' => array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{replace_me}/{optional}',
+					'port' => 80,
+				),
+				array('replace_me' => 'i-was-replaced', 'optional' => null),
+				'http://domain.tld/path/i-was-replaced/',
+			),
+			'parameter url encoding during unmasking' => array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{replace_me}',
+					'port' => 80,
+				),
+				array('replace_me' => 'i have wärd+ ßymbol$'),
+				'http://domain.tld/path/i%20have%20w%C3%A4rd%2B%20%C3%9Fymbol%24',
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider buildExceptionDataProvider
+	 * @expectedException \QATools\QATools\PageObject\Exception\MissingParametersException
+	 * @expectedExceptionMessage No parameters for "i_do_not_exist" masks given.
+	 */
+	public function testBuildParamException($normalized_components, array $params)
+	{
+		$url_builder = new Builder($normalized_components);
+		$url_builder->build($params);
+	}
+
+	public function buildExceptionDataProvider()
+	{
+		return array(
+			array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{i_do_not_exist}/{replace_me}',
+					'port' => 80,
+				),
+				array('replace_me' => 'i-was-replaced'),
+			),
+			array(
+				array(
+					'scheme' => 'http',
+					'host' => 'domain.tld',
+					'path' => '/path/{i_do_not_exist}/{replace_me}',
+					'port' => 80,
+				),
+				array('replace_me' => 'i-was-replaced', 'query' => 'string'),
+			),
 		);
 	}
 
